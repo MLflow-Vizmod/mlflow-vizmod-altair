ff36427b5c2c6613ecc0630467d63f7b0dbc633f
Hibbard, James <James.Hibbard@seattlechildrens.org>
2021-05-07 16:16:18 -0700

    bumped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 
 APP_NAME = "mlflow_vizmod.styles.altair"
-VERSION = "1.1.0"
+VERSION = "1.1.1"
 LICENSE = "MIT"
 AUTHOR = "James Hibbard, Andrew Bauman"
 DESCRIPTION = "MLflow Vizmod style for altair"
